098fe469a67d2196c30fec4cb4473bc90154605d
Frank Moreno <frankmoreno1993@gmail.com>
2020-03-16 10:20:38 -0500

    Updating examples for dialectical names

--- example/main.dart
+++ example/main.dart
@@ -2,16 +2,16 @@ import 'package:circular_buffer/circular_buffer.dart';
 
 main() async {
   CircularBuffer<int> cb = new CircularBuffer<int>(5);
-  
-  List<int> L = [4,5,1,-3,8,2,6,7,4,5];
+
+  List<int> L = [4, 5, 1, -3, 8, 2, 6, 7, 4, 5];
   int sum = 0;
   double mean;
-  for(int a in L){
-    int start = cb.filled? cb.start : 0;
+  for (int a in L) {
+    int first = cb.isFilled ? cb.first : 0;
     await cb.insert(a);
-    sum += cb.end - start;
+    sum += cb.last - first;
 
-    mean = sum.toDouble() / cb.len;
+    mean = sum.toDouble() / cb.length;
 
     print('Inserting ${a}:\tsum=$sum\tmean=$mean');
   }
