d12304e23a8968cb05fac3f3ffcf2a8d76c701af
Brett Sutton <bsutton@noojee.com.au>
2020-12-21 20:01:42 +1100

    With null saftey the list method can no longer be allocated with empty values. Code now manually tracks the capacity of the list so it can grow to the capacity but no more.

--- lib/circular_buffer.dart
+++ lib/circular_buffer.dart
@@ -1,51 +1,65 @@
+/// a circular buffer.
 class CircularBuffer<T> {
   List<T> _buf;
-  int _start;
-  int _end;
-  int _count;
+  int _start = 0;
+  int _end = -1;
+  int _count = 0;
+  final int capacity;
 
-  CircularBuffer(int n){
-    _buf = new List<T>(n);
-    reset();
-  }
+  /// create a circulare buffer with the ability
+  /// to store at most [capacity] items.
+  CircularBuffer(this.capacity) : _buf = <T>[] {}
 
-  reset(){
+  /// empty the buffer.
+  void reset() {
     _start = 0;
     _end = -1;
     _count = 0;
   }
 
-  insert(T el) async{
+  /// insert a value at the curent location.
+  void insert(T el) async {
     // Inserting the next value
     _end++;
-    if(_end == _buf.length){
+    if (_end == capacity) {
       _end = 0;
     }
     _buf[_end] = el;
 
     // updating the start
-    if(_count < _buf.length){
+    if (_count < capacity) {
       _count++;
       return;
     }
 
     _start++;
-    if(_start == _buf.length){
+
+    /// roll over and back to the start
+    if (_start == capacity) {
       _start = 0;
     }
   }
 
+  /// the first value in the buffer
   T get start => _buf[_start];
+
+  /// the last value in the buffer.
   T get end => _buf[_end];
 
+  /// the current lenght of the buffer
   int get len => _count;
-  int get cap => _buf.length;
 
-  bool get filled => (_count == _buf.length);
-  bool get unfilled => (_count < _buf.length);
+  /// the max capacity of the buffer.
+  int get cap => capacity;
+
+  /// true if the buffer is filled
+  bool get filled => (_count == capacity);
+
+  /// false if the buffer is not a capacity.
+  bool get unfilled => (_count < capacity);
 
   /// Allows you to iterate over the contents of the buffer
-  /// The [action] callback is called for each item in the 
+  /// The [action] callback is called for each item in the
   /// buffer.
   void forEach(void Function(T) action) {
     for (var i = _start; i < _start + _count; i++) {
