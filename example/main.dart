d29d483598655e34541dfe15565c2dd05c40f039
Frank Moreno <frankmoreno1993@gmail.com>
2021-01-23 12:32:57 -0500

    updating example to dart 2

--- example/main.dart
+++ example/main.dart
@@ -1,13 +1,13 @@
 import 'package:circular_buffer/circular_buffer.dart';
 
 main() async {
-  CircularBuffer<int> cb = new CircularBuffer(5);
+  final cb = CircularBuffer<int>(5);
 
-  List<int> L = [4, 5, 1, -3, 8, 2, 6, 7, 4, 5];
+  List<int> list = [4, 5, 1, -3, 8, 2, 6, 7, 4, 5];
   int sum = 0;
   double mean;
-  for (int a in L) {
-    int first = cb.isFilled ? cb.first : 0;
+  for (int a in list) {
+    final first = cb.isFilled ? cb.first : 0;
     cb.add(a);
     sum += cb.last - first;
 
