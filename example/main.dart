b2a9a26afea067f4c9af59769b4ebe501f492a74
Frank Moreno <frankmoreno1993@gmail.com>
2021-02-02 20:53:40 -0500

    chore: updating example following pedantic

--- example/main.dart
+++ example/main.dart
@@ -1,18 +1,18 @@
 import 'package:circular_buffer/circular_buffer.dart';
 
-main() async {
+void main() async {
   final cb = CircularBuffer<int>(5);
 
-  List<int> list = [4, 5, 1, -3, 8, 2, 6, 7, 4, 5];
-  int sum = 0;
+  final list = <int>[4, 5, 1, -3, 8, 2, 6, 7, 4, 5];
+  var sum = 0;
   double mean;
-  for (int a in list) {
+  for (var a in list) {
     final first = cb.isFilled ? cb.first : 0;
     cb.add(a);
     sum += cb.last - first;
 
     mean = sum.toDouble() / cb.length;
 
-    print('Inserting ${a}:\tsum=$sum\tmean=$mean');
+    print('Inserting $a:\tsum=$sum\tmean=$mean');
   }
 }
