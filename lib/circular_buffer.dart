cb3c88a3b782af9a33f0059a6a11851bedd432aa
Frank Moreno @kranfix <frankmoreno1993@gmail.com>
2024-09-27 15:47:24 -0500

    added len variable

--- lib/circular_buffer.dart
+++ lib/circular_buffer.dart
@@ -21,7 +21,8 @@ class CircularBuffer<T> with ListMixin<T> {
   /// Creates a [CircularBuffer] with a `capacity`
   CircularBuffer(this.capacity)
       : assert(capacity > 1, 'CircularBuffer must have a positive capacity.'),
-        _buf = [];
+        _buf = [],
+        _len = 0;
 
   /// Creates a [CircularBuffer] based on another `list`
   CircularBuffer.of(List<T> list, [int? capacity])
@@ -30,7 +31,8 @@ class CircularBuffer<T> with ListMixin<T> {
           'The capacity must be at least as long as the existing list',
         ),
         capacity = capacity ?? list.length,
-        _buf = [...list];
+        _buf = [...list],
+        _len = list.length;
 
   final List<T> _buf;
 
@@ -38,6 +40,7 @@ class CircularBuffer<T> with ListMixin<T> {
   final int capacity;
 
   int _start = 0;
+  int _len;
 
   /// Resets the [CircularBuffer].
   ///
@@ -45,6 +48,7 @@ class CircularBuffer<T> with ListMixin<T> {
   void reset() {
     _start = 0;
     _buf.clear();
+    _len = 0;
   }
 
   /// An alias to [reset].
@@ -57,6 +61,7 @@ class CircularBuffer<T> with ListMixin<T> {
       // The internal buffer is not at its maximum size.  Grow it.
       assert(_start == 0, 'Internal buffer grown from a bad state');
       _buf.add(element);
+      _len++;
       return;
     }
 
@@ -68,30 +73,42 @@ class CircularBuffer<T> with ListMixin<T> {
     }
   }
 
+  /// Adds an element as the first element
+  void addHead(T element) {
+    if (isFilled) {
+      if (_start == 0) {
+        _start = _len - 1;
+      } else {
+        _start--;
+      }
+      _buf[_start] = element;
+    }
+  }
+
   /// Number of used elements of [CircularBuffer]
   @override
-  int get length => _buf.length;
+  int get length => _len;
 
   /// The [CircularBuffer] `isFilled` if the [length]
   /// is equal to the [capacity].
-  bool get isFilled => _buf.length == capacity;
+  bool get isFilled => _len == capacity;
 
   /// The [CircularBuffer] `isUnfilled` if the [length] is
   /// less than the [capacity].
-  bool get isUnfilled => _buf.length < capacity;
+  bool get isUnfilled => _len < capacity;
 
   @override
   T operator [](int index) {
-    if (index >= 0 && index < _buf.length) {
-      return _buf[(_start + index) % _buf.length];
+    if (index >= 0 && index < _len) {
+      return _buf[(_start + index) % _len];
     }
     throw RangeError.index(index, this);
   }
 
   @override
   void operator []=(int index, T value) {
-    if (index >= 0 && index < _buf.length) {
-      _buf[(_start + index) % _buf.length] = value;
+    if (index >= 0 && index < _len) {
+      _buf[(_start + index) % _len] = value;
     } else {
       throw RangeError.index(index, this);
     }
